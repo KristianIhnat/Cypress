2ad6fa7dfb4f965cc6186e2d289f4ba2feeebae4
Kristian <tino.ihnat@gmail.com>
2023-02-22 15:13:34 +0100

    editing UIhandlingElements file

--- cypress/e2e/handlingUIElements2.cy.js
+++ cypress/e2e/handlingUIElements2.cy.js
@@ -22,8 +22,16 @@ describe('Suite Name', function (){
 
         cy.visit('https://demo.automationtesting.in/Register.html')
 
-        cy.get('#checkbox1').check().should('be.checked').and('have.value', 'Cricket')
-        cy.get('#checkbox2').check().should('be.checked').and('have.value', 'Movies')
+        cy.get('#checkbox1')
+          .check()
+          .should('be.checked')
+          .and('have.value', 'Cricket')
+        
+        cy.get('#checkbox2')
+          .check()
+          .should('be.checked')
+          .and('have.value', 'Movies')
+          
         cy.get('#checkbox3').check().should('be.checked').and('have.value', 'Hockey')
 
         cy.get('#checkbox1').uncheck().should('not.be.checked')
