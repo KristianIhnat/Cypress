7355ad82850a22752d4d554380f06f1a1708bfdc
Kristian <tino.ihnat@gmail.com>
2023-02-24 15:23:51 +0100

    added checkBoxes.cy.js

---
+++ cypress/e2e/checkBoxes.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="Cypress" />
+
+describe('checkBoxes', () => {
+    it('test1', () => {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+
+        cy.get('#checkBoxOption1').check().should('be.checked')
+          .and('have.value', 'option1')
+
+        cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
+
+        cy.get('input[type="checkbox"]').check(['option2', 'option3'])
+    })
+})
