42e06e989f5cc23973720b081f6fbaded25a4962
Kristian <tino.ihnat@gmail.com>
2023-02-26 17:26:39 +0100

    add another assertions to checkBoxes.cy.js

--- cypress/e2e/checkBoxes.cy.js
+++ cypress/e2e/checkBoxes.cy.js
@@ -3,12 +3,29 @@
 describe('checkBoxes', () => {
     it('test1', () => {
         cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
-
+        //checkboxex
         cy.get('#checkBoxOption1').check().should('be.checked')
           .and('have.value', 'option1')
 
         cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
 
         cy.get('input[type="checkbox"]').check(['option2', 'option3'])
+
+        //Static dropdown
+        cy.get('#dropdown-class-example').select('option2')
+          .should('have.value', 'option2')
+
+        //Dynamic dropdow
+        cy.get('#autocomplete').type('ind')  
+
+        cy.get('.ui-menu-item div')
+          .each(($el, index, $list) => {
+            if($el.text()==="India") {
+                cy.wrap($el).click()
+            }
+          
+        })
+
+        cy.get('#autocomplete').should('have.value', 'India')
     })
 })
