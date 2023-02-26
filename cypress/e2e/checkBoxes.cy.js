2fbe9585d752eb2e91b5b62e2a1b19afd742f424
Kristian <tino.ihnat@gmail.com>
2023-02-26 17:42:19 +0100

    added radio buttons to checkBoxes.cy.js

--- cypress/e2e/checkBoxes.cy.js
+++ cypress/e2e/checkBoxes.cy.js
@@ -6,18 +6,15 @@ describe('checkBoxes', () => {
         //checkboxex
         cy.get('#checkBoxOption1').check().should('be.checked')
           .and('have.value', 'option1')
-
         cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
-
         cy.get('input[type="checkbox"]').check(['option2', 'option3'])
 
         //Static dropdown
         cy.get('#dropdown-class-example').select('option2')
           .should('have.value', 'option2')
 
-        //Dynamic dropdow
+        //Dynamic dropdown
         cy.get('#autocomplete').type('ind')  
-
         cy.get('.ui-menu-item div')
           .each(($el, index, $list) => {
             if($el.text()==="India") {
@@ -27,5 +24,15 @@ describe('checkBoxes', () => {
         })
 
         cy.get('#autocomplete').should('have.value', 'India')
+
+        //Invisible elements
+        cy.get('#displayed-text').should('be.visible')
+        cy.get('#hide-textbox').click()
+        cy.get('#displayed-text').should('not.be.visible')
+        cy.get('#show-textbox').click()
+        cy.get('#displayed-text').should('be.visible')
+
+        //radio buttons
+        cy.get('input[value="radio2"]').check().should('be.checked')
     })
 })
